68644dd2bc62ed76157d30ff1dee51349195917a
Aashutosh Singh <aashutoshsingh@aashutoshs-macbook-pro.local>
2017-02-19 10:18:39 +0530

    Initial Commit

---
+++ MovieCrawler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovieCrawler
+//
+//  Created by Aashutosh Singh on 2/19/17.
+//  Copyright © 2017 Aashutosh Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MovieCrawler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MovieCrawler
+//
+//  Created by Aashutosh Singh on 2/19/17.
+//  Copyright © 2017 Aashutosh Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
